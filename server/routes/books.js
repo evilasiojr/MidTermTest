1ea121ed4baedeb9913241456961cdfea1ce1f9d
Evilasio Junior <evilasiojunior@hotmail.com>
2021-06-25 15:40:41 -0400

    first commit

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,3 +1,6 @@
+//File name: Midterm Summer 2021 - Comp 229
+//name: Alessandro da Silva Santos, Author's name: Alessandro da Silva Santos, StudentID: 300927354
+
 // modules required for routing
 let express = require('express');
 let router = express.Router();
@@ -9,99 +12,60 @@ let book = require('../models/books');
 /* GET books List page. READ */
 router.get('/', (req, res, next) => {
   // find all books in the books collection
-  book.find( (err, books) => {
+  book.find((err, books) => {
     if (err) {
       return console.error(err);
-    }
-    else {
+    } else {
       res.render('books/index', {
         title: 'Books',
         books: books
       });
     }
   });
-});
-module.exports.displayBookList = (req, res, next) => {
-  Book.find((err, bookList) => {
-      if(err)
-      {
-          return console.error(err);
-      }
-      else
-      {
-          //console.log(BookList);
 
-          res.render('book/list', 
-          {title: 'Book', 
-          BookList: bookList, 
-          displayName: req.user ? req.user.displayName : ''});      
-      }
-  });
-}
+});
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
+  res.render('books/details', {
+    title: 'Add a new Book',
+    books: {
+      Title: '',
+      Author: '',
+      Price: null,
+      Genre: '',
+    }
+  });
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-     module.exports.displayAddPage = (req, res, next) => {
-      res.render('book/add', {title: 'Add Book', 
-      displayName: req.user ? req.user.displayName : ''})          
-  }
-
-    /* GET Route for displaying the Add page - CREATE Operation */
-router.get('/add', displayAddPage);
 
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-     module.exports.processAddPage = (req, res, next) => {
-      let newBook = Book({
-          "name": req.body.name,
-          "author": req.body.author,
-          "published": req.body.published,
-          "description": req.body.description,
-          "price": req.body.price
-      });
-  
-      Book.create(newBook, (err, Book) =>{
-          if(err)
-          {
-              console.log(err);
-              res.end(err);
-          }
-          else
-          {
-              // refresh the book list
-              res.redirect('/book-list');
-          }
-      });
-  }
+  let newBook = book({
+    "Title": req.body.title,
+    "Price": req.body.price,
+    "Author": req.body.author,
+    "Genre": req.body.genre
+  });
+  book.create(newBook, (err, Book) =>{
+    if(err) {
+        console.log(err);
+        res.end(err);
+    } else {
+          res.redirect('/books');
+    }
+});
 
-    /* POST Route for processing the Add page - CREATE Operation */
-router.post('/add', processAddPage);
 });
 
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
-    /* GET Route for displaying the Edit page - UPDATE Operation */
-router.get('/edit/:id', req, displayEditPage);
-});
-module.exports.displayEditPage = (req, res, next) => {
   let id = req.params.id;
 
-  Book.findById(id, (err, bookToEdit) => {
+  book.findById(id, (err, updateBook) => {
       if(err)
       {
           console.log(err);
@@ -109,62 +73,41 @@ module.exports.displayEditPage = (req, res, next) => {
       }
       else
       {
-          //show the edit view
-          res.render('book/edit', {title: 'Edit Book', book: bookToEdit, 
-          displayName: req.user ? req.user.displayName : ''})
+          res.render('books/details', {title: 'Updating your Book', books: updateBook})
       }
   });
-}
+
+});
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-     module.exports.processEditPage = (req, res, next) => {
-      let id = req.params.id
-  
-      let updatedBook = Book({
-          "_id": id,
-          "name": req.body.name,
-          "author": req.body.author,
-          "published": req.body.published,
-          "description": req.body.description,
-          "price": req.body.price
-      });
-  
-      Book.updateOne({_id: id}, updatedBook, (err) => {
-          if(err)
-          {
-              console.log(err);
-              res.end(err);
-          }
-          else
-          {
-              // refresh the book list
-              res.redirect('/book-list');
-          }
-      });
-  }
-    /* POST Route for processing the Edit page - UPDATE Operation */
-    router.post('/edit/:id', processEditPage);
+  let id = req.params.id;
+
+  let bookUpdate = book({
+      "_id": id,
+      "Title": req.body.title,
+      "Author": req.body.author,
+      "Price": req.body.price,
+      "Genre": req.body.genre
+  });
+  book.updateOne({_id: id}, bookUpdate, (err) => {
+    if(err)
+    {
+        console.log(err);
+        res.end(err);
+    } else {
+        res.redirect('/books');
+    }
+});
+
 });
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
-    /* GET to perform  Deletion - DELETE Operation */
-    router.get('/delete/:id', performDelete);
-});
-module.exports.performDelete = (req, res, next) => {
   let id = req.params.id;
 
-  Book.remove({_id: id}, (err) => {
+  book.remove({_id: id}, (err) => {
       if(err)
       {
           console.log(err);
@@ -172,10 +115,9 @@ module.exports.performDelete = (req, res, next) => {
       }
       else
       {
-           // refresh the book list
-           res.redirect('/book-list');
+        res.redirect('/books');
       }
   });
-}
+});
 
 module.exports = router;
