962e6f2f69bffac56b5c1f00ce1af024fd3d03ab
Evilasio Junior <evilasiojunior@hotmail.com>
2021-06-25 16:22:43 -0400

    updated db URI to work with Heroku

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://evilasio:A7ECN65CHfJfBkwO@cluster0.zawke.mongodb.net/books?retryWrites=true&w=majority"
 };
